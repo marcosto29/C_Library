599961a58b36610f062e3fad0c91916b5b4af5b0
Marcos <marcos29diciembre@gmail.com>
2025-04-17 23:42:05 +0200

    all functions fixed

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: matoledo <matoledo@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/13 16:35:47 by matoledo          #+#    #+#             */
-/*   Updated: 2025/04/17 09:57:13 by matoledo         ###   ########.fr       */
+/*   Updated: 2025/04/17 23:39:23 by matoledo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ void	free_memory(char **splitted_word)
 	char	**pt_aux;
 
 	pt_aux = splitted_word;
-	while(*pt_aux)
+	while (*pt_aux)
 	{
-		free(pt_aux++);
+		free(*pt_aux++);
 	}
 	free(splitted_word);
 }
@@ -31,48 +31,68 @@ static unsigned int	ft_splits_number(const char *to_split, char split_character)
 	splits = 1;
 	while (*to_split)
 	{
-		if (*to_split++ == split_character)
+		if (*to_split == split_character)
+		{
 			splits++;
+			while (*to_split == split_character)
+			{
+				to_split++;
+			}
+			continue;
+		}
+		to_split++;
 	}
 	return (splits);
 }
 
-static char	**ft_split_word(char **split, char *to_split, char c)
+static unsigned int	ft_letters_counter(const char *to_split, char c)
 {
-	char			*pt_aux;
-	char			*pt_splitted;
 	unsigned int	letters;
 
-	if (!to_split)
-		return (split);
-	pt_aux = to_split;
 	letters = 0;
-	while (*pt_aux != c)
+	while (*to_split != c && *to_split)
 	{
-		pt_aux++;
 		letters++;
+		to_split++;
 	}
-	pt_splitted = ft_calloc(sizeof(char), (letters + 1));
-	ft_memcpy(pt_splitted, to_split, letters);
-	pt_aux = ft_strchr(to_split, c);
-	if (pt_aux)
-		pt_aux++;
-	*split++ = pt_splitted;
-	ft_split_word(split, pt_aux, c);
-	return (split);
+	return (letters);
+}
+
+static char	**ft_split_word(char **split, const char *to_split, char c)
+{
+	char			**pt_splitted;
+	char			*splitted;
+	unsigned int	letters;
+
+	if (!*to_split)
+		return (split);
+	pt_splitted = split;
+	letters = ft_letters_counter(to_split, c);
+	splitted = ft_calloc(sizeof(char), letters + 1);
+	if (!splitted)
+	{
+		free_memory(split);
+		return (0);
+	}
+	while (*to_split != c && *to_split)
+		*splitted++ = *to_split++;
+	*split++ = splitted - letters;
+	while (*to_split == c)
+		to_split++;
+	ft_split_word(split, to_split, c);
+	return (pt_splitted);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	char			*pt_aux;
 	char			**pt_return;
 	unsigned int	splits;
 
-	pt_aux = (char *)s;
+	s = ft_strtrim(s, &c);
 	splits = ft_splits_number(s, c);
-	pt_return = ft_calloc(sizeof(char *), (splits + 1));
+	pt_return = ft_calloc(sizeof(char *), splits + 1);
 	if (!pt_return)
 		return (0);
-	ft_split_word(pt_return, pt_aux, c);
+	pt_return = ft_split_word(pt_return, s, c);
 	return ((char **)pt_return);
 }
