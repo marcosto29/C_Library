c3419a51d802bbebf0d17b097cff4771ac015742
Marcos <marcos29diciembre@gmail.com>
2025-04-23 16:01:47 +0200

    atoi hot fix

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: matoledo <matoledo@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 13:00:42 by matoledo          #+#    #+#             */
-/*   Updated: 2025/04/17 10:03:12 by matoledo         ###   ########.fr       */
+/*   Updated: 2025/04/23 16:01:22 by matoledo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,23 @@
 
 int	ft_atoi(const char *nptr)
 {
-	int	counter;
 	int	number;
 	int	negative;
 
-	negative = 0;
-	counter = 1;
 	number = 0;
+	negative = 1;
 	while ((*nptr >= 9 && *nptr <= 13) || *nptr == 32)
 		nptr++;
 	if (*nptr == 43 || *nptr == 45)
 	{
 		if (*nptr == 45)
-			negative++;
+			negative = -1;
 		nptr++;
 	}
-	while (ft_isdigit(*(nptr + 1)) != 0 && ft_isdigit(*nptr) != 0)
-		nptr++;
-	while (ft_isdigit(*nptr) != 0)
+	while (ft_isdigit(*nptr))
 	{
-		number += counter * (*nptr-- - '0');
-		counter *= 10;
+		number = number * 10 + (*nptr - '0');
+		nptr++;
 	}
-	if (negative % 2 != 0)
-		number *= -1;
-	return (number);
+	return (number * negative);
 }
